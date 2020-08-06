18ae5828d82d69e98e335f4c93732c214b422d91
Pablo_Fraquia <pablofraquia@gmail.com>
2020-08-06 18:23:46 -0300

    second commit

---
+++ src/main/java/pageFactory/ObjectFactory.java
@@ -0,0 +1,10 @@
+package pageFactory;
+
+public class ObjectFactory {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/test/java/Setup/Driverconfig.java
+++ src/test/java/Setup/Driverconfig.java
@@ -1,9 +1,20 @@
 package Setup;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import utility.MainUtility;
+
 public class Driverconfig {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static WebDriver getChromeDriverInitializer() {
+		
+			System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"\\Executables\\chromedriver.exe");
+			System.setProperty("webdriver.chrome.silentOutput", "true");
+			WebDriver driver = new ChromeDriver(MainUtility.setOptions());
+			return driver;
+			
+		}
 
 	}
 
