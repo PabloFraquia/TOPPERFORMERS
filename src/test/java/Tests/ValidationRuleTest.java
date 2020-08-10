ba972b1de3a147a6947a32247c6b63c557b24e35
Pablo_Fraquia <pablofraquia@gmail.com>
2020-08-10 08:36:15 -0300

    algunas pruebas pero sigue fallando

--- src/test/java/Tests/ValidationRuleTest.java
+++ src/test/java/Tests/ValidationRuleTest.java
@@ -4,6 +4,7 @@ import static org.testng.Assert.assertEquals;
 
 import java.util.Properties;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
@@ -11,9 +12,11 @@ import org.testng.annotations.Test;
 
 import Setup.DriverConfig;
 import Setup.ValidationPropConfig;
+import constants.setup.objectManager.objectManagerConstants;
 import domain.Login;
 import domain.ValidationRule;
 import factory.GlobalFactory;
+import factory.ObjectManagerFactory;
 
 @Test
 public class ValidationRuleTest {
@@ -22,6 +25,9 @@ public class ValidationRuleTest {
 	String url = "https://login.salesforce.com";
 	String user = "pablofraquia@gmail.com";
 	String pass = "Selenium2020!";
+	String tabName = "Object Manager";
+	String objectName = "Account";
+	String detailName = "Validation Rules";
 	String validationRuleName;
 	String formula;
 	String wrongFormula;
@@ -37,20 +43,30 @@ public class ValidationRuleTest {
 		driver.get(url);
 		login = new Login(driver);
 		login.login(user, pass);
-		Thread.sleep(7000);
+		Thread.sleep(10000);
+		//vRule.goToValidationRules(tabName, objectName, detailName); // aca es donde esta saltando la falla 
+		
+		/* también intenté manual pero no encuentra los elementos
+		 
+		driver.findElement(By.xpath("//*[@title = 'Object Manager']")).click();
+		driver.findElement(By.xpath("//*[text() = 'Account']")).click();
+		driver.findElement(By.xpath("//*[text() = 'Validation Rules']")).click();
+		*/
 		
 	}
 	
 	@BeforeMethod
 	public void initValidationRuleProperties() {
+		
 		Properties prop = ValidationPropConfig.getValidationProperties();
 		validationRuleName = prop.getProperty("validationRuleName");
 		formula = prop.getProperty("formula");
 		wrongFormula = prop.getProperty("wrongFormula");
 		errorMessage = prop.getProperty("errorMessage");
+		
 	}
 	
-	@Test
+	@Test (priority = 0)
 	public void noRequiredField() throws InterruptedException {
 		vRule.validationRuleConstruction(validationRuleName, formula, "");
 		assertEquals(vRule.checkErrorDisplay(), " You must enter a value");
@@ -59,7 +75,7 @@ public class ValidationRuleTest {
 		
 	}
 	
-	@Test
+	@Test (priority = 1)
 	public void wrongFormulaText() throws InterruptedException {
 		vRule.validationRuleConstruction(validationRuleName, wrongFormula, errorMessage);
 		assertEquals(vRule.checkFormulaError(), " Syntax error.  Found 'Banana'");
@@ -67,10 +83,10 @@ public class ValidationRuleTest {
 		gf.getCancelButton().click();
 	}
 	
-	@Test
+	@Test (priority = 2)
 	public void correctFilledFields() {
 		vRule.validationRuleConstruction(validationRuleName, formula, errorMessage);
-		assertEquals(vRule.checkEditButton(), true);
+		//assertEquals(vRule.checkEditButton(), true);
 		
 	}
 
