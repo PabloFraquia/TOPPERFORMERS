ac2fd24caa76cef547d70298b69a94539cac86b2
HelaMachine <martinlopezribero@gmail.com>
2020-08-11 15:23:07 -0300

    Working validation rule

--- src/main/java/constants/setup/objectManager/objectManagerCapabilities/validationRulesConstants.java
+++ src/main/java/constants/setup/objectManager/objectManagerCapabilities/validationRulesConstants.java
@@ -2,11 +2,12 @@ package constants.setup.objectManager.objectManagerCapabilities;
 
 public class validationRulesConstants {
 	
-	public static final String validatioRuleName = "#ValidationName";
+	//public static final String validatioRuleName = "#ValidationName";
+	public static final String validatioRuleName = "/html/body/form/div/div[2]/div[2]/table/tbody/tr[2]/td[2]/div/input";
 	public static final String formulaText = ".FormulaText";
 	public static final String validatioMessage = "#ValidationMessage";
 	public static final String errorFormulaMsg = ".errorMsg";
-	public static final String errorMessage = "#errorDiv_ep";
+	public static final String ErrorInvalidData = "#errorDiv_ep";
 	public static final String editButton = "//*[@id=\"topButtonRow\"]/input[1]";
 
 }
